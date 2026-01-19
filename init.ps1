24e56a2a469284f4835d2799cf004c1a5f5403c1
i314q159 <i314q159@outlook.com>
2026-01-19 14:51:02 +0800

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,5 +1,11 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-wsl --update
+
+# 微软服务器
+# wsl --update
+
+# GitHub Release
+wsl --update --web-download
+
 wsl --set-default-version 2
 wsl --list --online
